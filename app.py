23c5d39b50b167474476ca723810fcee2752d6fb
shailendra suman <shailendra.suman@smarsh.com>
2024-10-24 16:39:51 +0530

    [APPSERV-15780] change in the app code

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+
+def main():
+    # Fetch the password from environment variables
+    password = os.getenv('MY_PASSWORD')
+    
+    if password:
+        print(f"Hello, World! The fetched password is: {password}")
+    else:
+        print("Hello, World! No password found.")
+
+if __name__ == "__main__":
+    main()
+
