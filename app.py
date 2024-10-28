77c0b9449a106e538d56547cab1dd3a65bbd4826
shailendra suman <shailendra.suman@smarsh.com>
2024-10-28 18:34:37 +0530

    [APPSERV-15780]-helo-world-take3

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
+# app.py
 import os
 
 def main():
-    # Fetch the password from environment variables
-    password = os.getenv('MY_PASSWORD')
-    
-    if password:
-        print(f"Hello, World! The fetched password is: {password}")
-    else:
-        print("Hello, World! No password found.")
+    # Read template from ConfigMap
+    with open("/config/template", "r") as file:
+        template = file.read()
+
+    # Read password from externalsecret
+    with open("/secret/password", "r") as file:
+        password = file.read().strip()
+
+    # Replace .password in the template
+    message = template.replace(".password", password)
+    print(message)
 
 if __name__ == "__main__":
     main()
+
 
 
